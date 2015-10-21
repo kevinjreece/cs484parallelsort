685d600b74d7bbee3089c0688f90e90ebd0977bd
Kevin J Reece <kevinjreece@gmail.com>
2015-10-20 23:49:52 -0600

    Initial commit. Communication structure is correct

---
+++ sort.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "mpi.h"
+
+int main(int argc, char* argv[]) {
+	// for each dimension:
+		// find world pivot
+		// split data
+		// find partner in world
+		// send data to partner
+		// receive data from partner
+
+    int proc_id;
+    int n_proc;
+
+    MPI_Init(&argc, &argv);
+    MPI_Comm_size(MPI_COMM_WORLD, &n_proc);
+    MPI_Comm_rank(MPI_COMM_WORLD, &proc_id);
+
+    int dims = log2(n_proc);
+    MPI_Comm comms[dims + 1];
+    int rank[dims];
+    int world_size[dims];
+    int pivot[dims];
+    int group_id[dims];
+
+    comms[0] = MPI_COMM_WORLD;
+
+    int i;
+    for (i = 0; i < dims; i++) {
+    	MPI_Comm_size(comms[i], &world_size[i]);
+    	MPI_Comm_rank(comms[i], &rank[i]);
+
+    	pivot[i] = proc_id;
+    	MPI_Bcast(&pivot[i], 1, MPI_INT, 0, comms[i]);
+
+	    group_id[i] = rank[i] < (world_size[i] / 2) ? 0 : 1;
+
+    	printf("dim: %d\tid: %d\trank: %d\tpivot: %d\tgroup: %d\n", i, proc_id, rank[i], pivot[i], group_id[i]);
+
+	    MPI_Comm_split(comms[i], group_id[i], rank[i], &comms[i + 1]);
+
+	}
+
+    MPI_Finalize();
+
+    return 0;
+}
